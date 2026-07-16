97b0a8d68ac3af204d0d6925d064b8bd9ef95405
botaniy42-eng <botaniy42@gmail.com>
2026-07-16 23:45:08 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const bot = mineflayer.createBot({
     host: config.host || 'arafat602.minefort.com',
     port: config.port || 19132,
-    username: config.username || 'AnikaBot',
+    username: config.username || 'Anika',
     version: config.version || false, // সার্ভারের ভার্সন অনুযায়ী এটি অটো ডিটেক্ট করবে
     auth: 'offline' 
 });
