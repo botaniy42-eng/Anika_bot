b0c50b1dc5bc97a8f2dcecaea8f1e03afa3b3b7b
botaniy42-eng <botaniy42@gmail.com>
2026-07-16 19:33:52 +0600

    Update stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -1,1 +1,14 @@
+module.exports = {
+    name: 'stop',
+    description: 'বটের সব চলমান কাজ (যেমন হাঁটা বা ফলো করা) বন্ধ করবে',
+    execute(bot, sender, args) {
+        bot.clearControlStates();
+        
+        if (bot.currentFollowInterval) {
+            clearInterval(bot.currentFollowInterval);
+            bot.currentFollowInterval = null;
+        }
 
+        bot.chat(`সব কাজ বন্ধ করা হলো, ${sender}! আমি এখন শান্ত হয়ে দাঁড়িয়ে আছি।`);
+    }
+};
